3936d77d2f9036bda83748cb7be437b4fbce4e0e
sharonkorir <sharon.korir@student.moringaschool.com>
2022-11-15 16:05:37 +0300

    Feature: create and style navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,39 @@
+import metabnb from '../assets/metabnb.png'
+
 function Navbar(){
   return(
-    <div>Navbar works</div>
+    <div className="lg:px-20">
+      <div className="navbar bg-base-100">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+          </label>
+          <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+            <li><a href="/">Home</a></li>
+            <li><a href="/">Place to stay</a></li>
+            <li><a href="/">NFTs</a></li>
+            <li><a href="/">Community</a></li>
+          </ul>
+        </div>
+        <a href='/'>
+          <img alt="metabnb logo" src={metabnb} className="max-h-5"/>
+        </a>
+        
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal p-0">
+          <li><a href="/">Home</a></li>
+          <li><a href="/">Place to stay</a></li>
+          <li><a href="/">NFTs</a></li>
+          <li><a href="/">Community</a></li>
+        </ul>
+        </div>
+        <div className="navbar-end">
+          <a className="btn normal-case" id='wallet' href='/'>Connect wallet</a>
+        </div>
+      </div>
+    </div>
   )
 }
 
