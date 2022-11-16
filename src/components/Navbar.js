241f94ba673d8986106b984d9b7bdbbcc8cc98dd
sharonkorir <sharon.korir@student.moringaschool.com>
2022-11-16 14:08:37 +0300

    Assets: create places images

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -29,7 +29,17 @@ export function Navbar(){
         </ul>
         </div>
         <div className="navbar-end">
-          <a className="btn normal-case w-44" id='wallet' href='/'>Connect wallet</a>
+          <a className="btn normal-case w-44" id='wallet' href="#my-modal-2">Connect wallet</a>
+          {/* The button to open modal */}
+          <div className="modal" id="my-modal-2">
+            <div className="modal-box">
+              {/* <h3 className="font-bold text-lg">Congratulations random Internet user!</h3>
+              <p className="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+              <div className="modal-action">
+              <a href="#" className="btn">Yay!</a>
+              </div> */}
+            </div>
+          </div>
         </div>
       </div>
     </div>
