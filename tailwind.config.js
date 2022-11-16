377c0840b53f334847ab56f6999cb4fbbb5c4af4
sharonkorir <sharon.korir@student.moringaschool.com>
2022-11-16 11:38:58 +0300

    Styles: import and set up font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,7 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {
-      fontFamily: ['Red Rose', 'cursive']
-    },
+    extend: {},
   },
   plugins: [require("daisyui")],
 }
