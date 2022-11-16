f4497bf25fbc9b6c79675d66c9673525c30c29d4
sharonkorir <sharon.korir@student.moringaschool.com>
2022-11-16 16:04:36 +0300

    Fix: make filter tabs responsive

---
+++ src/components/PlacesToStay.js
@@ -0,0 +1,34 @@
+export const PlaceToStayFilters = [
+	{
+		name: "Restaurant",
+		link: "/",
+	},
+	{
+		name: "Cottage",
+		link: "/",
+	},
+	{
+		name: "Castle",
+		link: "/",
+	},
+	{
+		name: "Fantast City",
+		link: "/",
+	},
+	{
+		name: "Beach",
+		link: "/",
+	},
+	{
+		name: "Carbins",
+		link: "/",
+	},
+	{
+		name: "Off-grid",
+		link: "/",
+	},
+	{
+		name: "Farm",
+		link: "/",
+	},
+];
--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -1,45 +1,27 @@
 import React, { Component } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSliders } from "@fortawesome/free-solid-svg-icons";
+import Filter from '../assets/filterIcon.png'
+
+import { PlaceToStayFilters } from "./PlacesToStay";
+import { Link } from "react-router-dom";
 
 class Tabs extends Component {
   state = {};
   render() {
     return (
       <React.Fragment>
-        <div className="flex justify-center mb-8">
-          <div className="w-2/12 flex justify-center">
-            <span className="text-gray-700 hover:cursor-pointer hover:text-blue-500 hover:underline">
-              Restaurant
-            </span>
+        <div className="py-8 lg:py-[3rem] px-2 lg:px-8 max-w-[1200px] mx-auto">
+			    <div className="flex flex-col lg:flex-row items-center text-sm lg:w-[90%] mx-auto mb-[2rem]">
+				    <div className="flex justify-between flex-nowrap w-full overflow-x-scroll md:scrollbar-hide scroll-smooth mb-4 lg:mb-0 py-4">
+					    {PlaceToStayFilters.map((item, id) => (
+                <Link key={id} to={item.link} className='text-xs whitespace-nowrap mx-4'>{item.name}</Link>
+              ))}
+            </div>
+            <div className="ml-10 flex items-center border border-[#B4B4B4] px-4 py-1 rounded-[4px]">
+                <span className="mr-6 text-xs">Location</span>
+                <img src={Filter} alt="Filter" className="h-[15px]"/>
+            </div>
           </div>
-          <div className="w-2/12 flex justify-center">
-            <span className="text-gray-700 hover:cursor-pointer hover:text-blue-500 hover:underline">
-              Cottage
-            </span>
-          </div>
-          <div className="w-2/12 flex justify-center">
-            <span className="text-gray-700 hover:cursor-pointer hover:text-blue-500 hover:underline">
-              Castle
-            </span>
-          </div>
-          <div className="w-2/12 flex justify-center">
-            <span className="text-gray-700 hover:cursor-pointer hover:text-blue-500 hover:underline">
-              Beach
-            </span>
-          </div>
-          <div className="w-2/12 flex justify-center">
-            <span className="text-gray-700 hover:cursor-pointer hover:text-blue-500 hover:underline">
-              Off-Grid
-            </span>
-          </div>
-          <div className="w-2/12 flex justify-center">
-            <button className="border border-gray-500 text-gray-900 px-2 py-1 rounded">
-              <span className="font-bold mr-2">Location</span>
-              <FontAwesomeIcon icon={faSliders}></FontAwesomeIcon>
-            </button>
-          </div>
-        </div>
+			</div>
       </React.Fragment>
     );
   }
